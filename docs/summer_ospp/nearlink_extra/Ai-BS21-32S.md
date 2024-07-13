7a236f5f8f1e53534aaf37d0d40038c50bfca524
Abrillant Lee <haonan.brillant@gmail.com>
2024-07-13 10:36:00 +0800

    docs: add detailed nearlink-extra documentation

---
+++ docs/summer_ospp/nearlink_extra/Ai-BS21-32S.md
@@ -0,0 +1,13 @@
+---
+id: nearlink-extra-Ai-BS21-32S
+slug: nearlink-extra-Ai-BS21-32S
+title: 安信可Ai-BS21-32S
+authors: nan
+keywords:
+  - 星闪
+---
+
+## 其他
+ #### 4. 使用CH32V307VCT6的USART外设发送AT指令进行星闪通信
+  - [使用CH32V307VCT6的USART外设发送AT指令进行星闪通信](https://github.com/Fangling-Kare/NearLink)
+  - [试验文档](https://github.com/Pete-Young/pete-young.github.io/blob/main/Nearlink.docx)
--- sidebars.ts
+++ sidebars.ts
@@ -62,16 +62,22 @@ const sidebars: SidebarsConfig = {
     {
       label: '其他星闪开发板',
       type: 'category',
-      link: {
-        type: 'generated-index', //生成索引
-        title: '其他星闪开发板',
-        description:
-          "其他星闪开发板的使用案例",
-        keywords: ['guides'],
-        image: '/img/logo.png',
+      link:{
+        type: 'doc',
+        id: 'summer_ospp/nearlink_extra/nearlink-extra-guides',
       },
+      // link: {
+      //   type: 'generated-index', //生成索引
+      //   title: '其他星闪开发板',
+      //   description:
+      //     "其他星闪开发板的使用案例",
+      //   keywords: ['guides'],
+      //   image: '/img/logo.png',
+      // },
       items: [
-        'summer_ospp/nearlink_extra/nearlink-extra-guides'
+        'summer_ospp/nearlink_extra/nearlink-extra-ws63e',
+        'summer_ospp/nearlink_extra/nearlink-extra-hi2821',
+        'summer_ospp/nearlink_extra/nearlink-extra-Ai-BS21-32S'
       ],
     },
     {
